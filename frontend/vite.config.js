9e5be03acde863db9fb0d7dab192f76490dbadca
KevinBolanosDev <im.kevinbolanos.dev@gmail.com>
2025-03-17 20:35:37 -0500

    Correction base link to deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/articles/',
+  base: '/articles',
   server: {
     port: 5173,
     open: '/articles'
